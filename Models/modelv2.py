19b89272f4568e52897273bd10e4a711e10fb2a9
LizLima <lizbethlima@gmail.com>
2019-11-25 17:18:18 -0500

    Test1: Vgg19_bn(pretrained) | BCE(minmax-GAN)

--- Models/modelv2.py
+++ Models/modelv2.py
@@ -9,21 +9,18 @@ class Encoder(nn.Module):
         self.dim = 256
 
         # Endoder
-        self.encoder =  models.vgg19_bn(pretrained=True)
-         # change last layer input(4096) -> output (256)
         # VGG
+        self.encoder =  models.vgg19_bn(pretrained=True)
         self.n_infeatures = self.encoder.classifier._modules['6'].in_features
         self.encoder.classifier._modules['6'] = nn.Linear(self.n_infeatures, self.dim)
-        # self.fc = nn.Linear(self.dim, num_class)
-
+       
         # RESNET50
         # self.encoder =  models.resnet50(pretrained=True)
         # num_ftrs = self.encoder.fc.in_features
         # self.encoder.fc = nn.Linear(num_ftrs, self.dim)
 
     def forward(self, input):
-        out_space   = self.encoder(input) # latent space
-
+        out_space   = self.encoder(input)
         return out_space
 
 class Decoder(nn.Module):
@@ -31,9 +28,6 @@ class Decoder(nn.Module):
     def __init__(self,):
         super().__init__()
         self.dim = 256
-
-        # Decoder based on DCGAN
-        # self.fc_dec = nn.Linear(in_features=self.dim, out_features=4*4*128*8)
 
         self.conv0 = nn.ConvTranspose2d( self.dim, 128, 4, 1, 0, bias=False)
         self.bndc0 = nn.BatchNorm2d(128)
