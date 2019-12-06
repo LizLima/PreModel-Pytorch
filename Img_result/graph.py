445dfb6422e663e875d026fca4fd0e442ad9e04d
LizLima <lizbethlima@gmail.com>
2019-12-06 07:48:03 -0500

    Update parameters V1

--- Img_result/graph.py
+++ Img_result/graph.py
@@ -15,18 +15,18 @@ def main():
 def compare():
 
     # Load pickle file
-    path = '/media/liz/Files/Model-Pretrained/GAN_64batch_'
+    path = '/media/liz/Files/Model-Pretrained/GAN_loss'
 
     list_file = [
-        ("Test1", "Test1/files_gan_train.pckl"),
+        ("Test1", "/files_gan_train_999.pckl")
         # ("Test2", "Test2/files_gan_train.pckl"),
-        ("Test3", "Test3/files_gan_train_499.pckl"),
-        ("Test4", "Test4/files_gan_train_499.pckl"),
-        ("Test5", "Test5/files_gan_train_499.pckl"),
-        ("Test5_v1", "Test5_v1/files_gan_train_224.pckl"),
-        ("Test6", "Test6/files_gan_train_499.pckl"),
-        ("TestX_v1", "TestX_v1/files_gan_train_149.pckl"),
-        ("Test7", "Test7/files_gan_train_499.pckl"),
+        # ("Test3", "Test3/files_gan_train_499.pckl"),
+        # ("Test4", "Test4/files_gan_train_499.pckl"),
+        # ("Test5", "Test5/files_gan_train_499.pckl"),
+        # ("Test5_v1", "Test5_v1/files_gan_train_224.pckl"),
+        # ("Test6", "Test6/files_gan_train_499.pckl"),
+        # ("TestX_v1", "TestX_v1/files_gan_train_149.pckl"),
+        # ("Test7", "Test7/files_gan_train_499.pckl"),
     ]
     
     # Plot information
@@ -44,14 +44,14 @@ def compare():
 def compare_gan():
 
     # Load pickle file
-    path = '/media/liz/Files/Model-Pretrained/GAN_64batch_'
+    path = '/media/liz/Files/Model-Pretrained/GAN_loss'
 
     list_file = [
-        ("Test1", "Test1/files_gan_train.pckl"),
+        ("Test1", "/files_gan_train_999.pckl")
         # ("Test2", "Test2/files_gan_train.pckl"),
-        ("Test3", "Test3/files_gan_train_499.pckl"),
-        ("Test4", "Test4/files_gan_train_499.pckl"),
-        ("Test5", "Test5/files_gan_train_499.pckl"),
+        # ("Test3", "Test3/files_gan_train_499.pckl"),
+        # ("Test4", "Test4/files_gan_train_499.pckl"),
+        # ("Test5", "Test5/files_gan_train_499.pckl"),
         # ("Test5_v1", "Test5_v1/files_gan_train_224.pckl"),
         # ("Test6", "Test6/files_gan_train_499.pckl"),
         # ("TestX_v1", "TestX_v1/files_gan_train_149.pckl"),
@@ -63,9 +63,9 @@ def compare_gan():
             l = pickle.load(f) # List o values
             listG, listD = zip(*l)
 
-            # list_G = [ round(float(x), 2) for x in listG[1:] ]
-            # x = np.arange(len(list_G))
-            # plt.plot(x, list_G, label=label + "G")
+            list_G = [ round(float(x), 2) for x in listG[1:] ]
+            x = np.arange(len(list_G))
+            plt.plot(x, list_G, label=label + "G")
 
             list_D = [ round(float(x), 2) for x in listD[1:] ]
             x = np.arange(len(list_D))
